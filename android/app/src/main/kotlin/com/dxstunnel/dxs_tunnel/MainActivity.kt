311b714a34150be059930c718d749c7da672c96e
META JEADY <djiogofoyet@gmail.com>
2026-08-30 20:44:42 +0100

    Add MainActivity for Flutter integration

---
+++ android/app/src/main/kotlin/com/dxstunnel/dxs_tunnel/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.dxstunnel.dxs_tunnel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // Liaison native gérée dynamiquement par le moteur wireguard_flutter
+}
